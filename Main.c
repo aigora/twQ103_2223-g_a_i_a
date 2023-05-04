d78280f4d0132d9620815675b1b1ee181afae0c6
anarecio02 <ana.recio@alumnos.upm.es>
2023-05-04 10:21:40 +0200

    update del main

--- Main.c
+++ Main.c
@@ -1,20 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
+
+// funcion media
+// funcion mayor parametro
+// funcion menor parametro
 
 int main(){
 	
 	int opcion;
 	
 	printf("Bienvenido a GAIA, seleccione unos de los 4 barrios que tenemos disponibles para consultar sus datos\n");
-	printf("Pulse 1 para Atocha\n");
-	printf("Pulse 2 para Lavapies\n");
-	printf("Pulse 3 para \n");
-	printf("Pulse 4 para \n");
+	printf("Pulse [1] para Atocha\n");
+	printf("Pulse [2] para Lavapies\n");
+	printf("Pulse [3] para \n");
+	printf("Pulse [4] para \n");
 	printf("Pulse cualquier otro numero para salir\n");
 	
 	scanf("%d",&opcion);
+	
 	if (opcion==1){
 		printf("Ha seleccionado Atocha\n");
-		printf("A continuacion se abrira un fichero con los datos de este municipio\n");
+		 printf("A continuacion se abrira un fichero con los datos de este municipio\n");
+		 FILE * fentrada;
+		  fentrada = fopen("Atocha.txt", "r");
+		  if(fentrada == NULL){
+		  printf("Error en la apertura del fichero\n");
+		  return 0;
+		  }
 	} else if (opcion==2){
 		printf("Ha seleccionado Lavapies\n");
 		printf("A continuacion se abrira un fichero con los datos de este municipio\n");
